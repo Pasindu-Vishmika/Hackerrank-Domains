3ecbae7e3e5c48295f11a2aaf85d371faa0d4771
Adithya Sandew <92386905+adithyasandew@users.noreply.github.com>
2024-01-31 22:44:06 +0530

    Delete Linux Shell/007. Getting_Started_with_conditionals.sh

--- Linux Shell/007. Getting_Started_with_conditionals.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read char
-
-if [ "${char,,}" == "y" ]; then #convert the input to lowercase using ${char,,}
-    echo "YES"
-elif [ "${char^^}" == "N" ]; then # Or convert the input to uppercase using ${char^^} and compare with capital N
-    echo "NO"
-fi
