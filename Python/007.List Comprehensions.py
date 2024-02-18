0f2721d44601e99f4c58eacece2963dff29402ba
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-02-18 20:29:48 +0530

    fix test case

--- Python/007.List Comprehensions.py
+++ Python/007.List Comprehensions.py
@@ -3,12 +3,5 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    
-    my_list = []
-    for i in range(x + 1):
-        for j in range(y + 1):
-            for k in range(z + 1):
-                if i + j + k != n:
-                    my_list.append([i, j, k])
-
+    my_list =[[i,j,k] for i in range (x+1) for j in range (y+1) for k in range (z+1) if i+j+k != n]
     print(my_list)
