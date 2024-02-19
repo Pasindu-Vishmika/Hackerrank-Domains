0bd97d6913d7cb2c39f95e40da85f78096509a7b
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-02-19 09:29:26 +0530

    added 9th solution

---
+++ Python/009.Nested Lists.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    records = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name,score])
+    m = sorted(set([i[1] for i in records]))[1]
+    records.sort()
+    for n, s in records:
+        if s == m:
+            print(n)
